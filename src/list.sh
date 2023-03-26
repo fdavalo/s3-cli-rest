095629cc93e5981bdd533ab1c49cffc490181db5
fdavalo <57140104+fdavalo@users.noreply.github.com>
2023-03-26 19:52:19 +0200

    Update list.sh

--- src/list.sh
+++ src/list.sh
@@ -1,3 +1,4 @@
+#sh list.sh <access key> <secret key> <bucket name> / https://s3-openshift-storage.apps.bm3.redhat.hpecic.net 
 export AWS_ACCESS_KEY_ID=$1
 export AWS_SECRET_ACCESS_KEY=$2
 BUCKET=$3
