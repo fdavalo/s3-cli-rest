fcd10fde83d019c3c8963898c99de26029b6977b
fdavalo <57140104+fdavalo@users.noreply.github.com>
2023-03-27 09:32:48 +0200

    Update list.sh

--- src/list.sh
+++ src/list.sh
@@ -4,4 +4,4 @@ export AWS_SECRET_ACCESS_KEY=$2
 BUCKET=$3
 FILE=$4
 export URL=$5
-sh s3SimpleUrl.sh get s3://${BUCKET}${FILE} | python3 xml2json.py | jq -r '.ListBucketResult.Contents[] | [.LastModified, .Key] | @tsv'
+sh s3SimpleUrl.sh get s3://${BUCKET}${FILE} | python3 xml2json.py | jq -r '.ListBucketResult.Contents[] | [.LastModified, .Size, .Key] | @tsv' | sort
